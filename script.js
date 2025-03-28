bf34dfcd540db74d877f30750afb98e8c5e3bc39
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2025-03-28 12:04:52 +0530

    Projectdone

---
+++ script.js
@@ -0,0 +1,36 @@
+const search_input = document.querySelector(".search_field-input input"),
+  clear = document.querySelector(".clear"),
+  search_btn = document.querySelector("#search_btn");
+
+let search_text = search_input.addEventListener("keyup", (e) => {
+  //   console.log(e.key);
+  //   console.log(search_input.value);
+
+  search_text = search_input.value;
+
+  if (search_text.length >= 1) {
+    clear.style.display = "block";
+  } else {
+    clear.style.display = "none";
+  }
+
+  if (e.key == "Enter") {
+    googleSearch();
+  }
+});
+
+clear.onclick = () => {
+  clear.style.display = "none";
+  search_input.value = "";
+};
+
+search_btn.addEventListener("click", () => {
+  googleSearch();
+});
+
+function googleSearch() {
+  if (search_text.length >= 1) {
+    let url = `https://google.com/search?q=${search_text}`;
+    window.location = url;
+  }
+}
